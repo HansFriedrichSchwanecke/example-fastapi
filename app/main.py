d00b8aa067f4a95ba8f9b88b470a5d2bd4417051
fried <Friedrich.Schwanecke@web.de>
2022-01-24 22:51:53 +0100

    Deploy and Build Pipleline

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from app.routers import post, user, auth, vote
 
 from . import models
 from .database import engine
-models.Base.metadata.create_all(bind=engine)
+#models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(post.router)
